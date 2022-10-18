85f40fe0c396d73b25b09fe83373b038ea430794
OILSLICC <LachiKent@gmail.com>
2022-10-18 15:56:16 +1000

    Create pwm.c

---
+++ src/pwm.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <stdio.h>
+
+
+void pwm_init(){
+    PORTB.DIRSET = PIN1_bm;
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc;
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP1_bm | TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
+    TCA0.SINGLE.PER = 255;
+    TCA0.SINGLE.CMP1 = 255;
+    TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+}
+
+void brightness_adjust(uint8_t input){
+    TCA0.SINGLE.CMP1 = input;
+}
+
